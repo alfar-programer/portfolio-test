f867a07e6fd36683d89914dc1dbfd78c51dc3315
alfar-programer <mazenalfar01@gmail.com>
2025-06-19 23:55:58 +0300

    Initial commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,10 +29,10 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
-  { value: 90, suffix: "%", label: "Client Retention Rate" },
+  { value: 5, suffix: "+", label: "Years of Experience" },
+  { value: 20, suffix: "+", label: "Satisfied Clients" },
+  { value: 45, suffix: "+", label: "Completed Projects" },
+  { value: 10, suffix: "%", label: "Client Retention Rate" },
 ];
 
 const logoIconsList = [
